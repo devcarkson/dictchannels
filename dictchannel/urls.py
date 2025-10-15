3ce6b516f27e68d2db76bd1420b59309ea224b6e
devcarkson <decarkson@gmail.com>
2025-10-16 00:12:47 +0100

    sitemap

--- dictchannel/urls.py
+++ dictchannel/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('sitemap.xml', serve, {'document_root': settings.BASE_DIR, 'path': 'sitemap.xml'}),
 ]
 
 # Serve media and static files during development
